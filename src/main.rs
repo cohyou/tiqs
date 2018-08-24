6bf5d092d5158b767cfb291cb9ca7acaf4058ee3
cohyou <cohyou@ironoir.io>
2018-08-24 14:27:43 +0900

    change structure of arrows

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
+use std::collections::HashMap;
+
+type Arrows = HashMap<String, (String, String)>;
+
 struct Category {
     objects: Vec<String>,
-    arrows: Vec<String>
+    arrows: Arrows
+}
+
+fn insert_arrow(arrows: &mut Arrows, name: &str, domain: &str, codomain: &str) {
+    arrows.insert(name.to_string(), (domain.to_string(), codomain.to_string()));
 }
 
 fn main() {
+    let mut arrows = HashMap::new();
+    insert_arrow(&mut arrows, "f", "A", "B");
+    insert_arrow(&mut arrows, "g", "A", "C");
+    insert_arrow(&mut arrows, "h", "B", "C");
+
     let category = Category {
         objects: vec![
             "A".to_string(),
             "B".to_string(),
             "C".to_string()
         ],
-        arrows: vec![
-            "f: A -> B".to_string(),
-            "g: A -> C".to_string(),
-            "h: B -> C".to_string()
-        ]
+        arrows: arrows
     };
 }
