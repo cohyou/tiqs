cf45bc800228e15d886d29c0ae39aa8dd9d14602
cohyou <cohyou@ironoir.io>
2018-08-30 17:58:40 +0900

    make Category trait and add 0-cat

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,81 @@
-macro_rules! cat {
-    ([ $( $object:ident )* ] [ $( $arrow_name:ident : $domain:ident -> $codomain:ident )* ]) => ({
-        let mut objects = Vec::new();
-        $( objects.push(stringify!($object)); )*
-        let mut arrows = Vec::new();
-        $( arrows.push((stringify!($arrow_name), stringify!($domain), stringify!($codomain))); )*
-        new_category(objects, arrows)
-    })
-}
+use std::cmp::PartialEq;
+use std::collections::HashSet;
 
-use std::fmt::Display;
-use std::collections::HashMap;
+trait Category {
+    type Object: PartialEq;
+    type Arrow: PartialEq;
 
-type Arrows = HashMap<String, (String, String)>;
-
-struct Category {
-    objects: Vec<String>,
-    arrows: Arrows
-}
-
-impl Category {
-    fn has_arrow(&self, domain: &str, codomain: &str) -> bool {
-        for (dom, cod) in self.arrows.values() {
-            if dom == domain && cod == codomain {
-                return true
-            }
+    fn domain(&self, f: &Self::Arrow) -> Self::Object;
+    fn codomain(&self, f: &Self::Arrow) -> Self::Object;
+    fn identity(&self, a: Self::Object) -> Self::Arrow;
+    fn composition(&self, f: Self::Arrow, g: Self::Arrow) -> Option<Self::Arrow> {
+        if self.codomain(&f) != self.domain(&g) {
+            None
+        } else {
+            Some(self.composition_internal(&f, &g))
         }
-        false
+    }
+    fn composition_internal(&self, f: &Self::Arrow, g: &Self::Arrow) -> Self::Arrow;
+    fn ci(&self, f: &Self::Arrow, g: &Self::Arrow) -> Self::Arrow {
+        self.composition_internal(f, g)
     }
 
-    fn is_isomorphism(&self, object1: &str, object2: &str) -> bool {
-        self.has_arrow(object1, object2) && self.has_arrow(object2, object1)
+    fn objects() -> HashSet<Self::Object>;
+    fn arrows() -> HashSet<Self::Arrow>;
+
+    fn associativity(&self, f: Self::Arrow, g: Self::Arrow, k: Self::Arrow) -> bool {
+        self.ci(&self.ci(&f, &g), &k) == self.ci(&f, &self.ci(&g, &k))
+    }
+    fn unit_law_domain(&self, o: Self::Object, f: Self::Arrow) -> bool {
+        self.ci(&self.identity(o), &f) == f
+    }
+    fn unit_law_codmain(&self, o: Self::Object, f: Self::Arrow) -> bool {
+        self.ci(&f, &self.identity(o)) == f
     }
 }
 
-impl Display for Category {
-    fn fmt(&self, dest: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let mut arrow_texts = vec![];
-        for (k, v) in &self.arrows {
-            arrow_texts.push(format!("{}: {} -> {}", k, v.0, v.1));
-        }
-        write!(dest, "objects: {:?} \n arrows: {:?}", self.objects, arrow_texts)
-    }
+trait Functor {
+    type C: Category;
+    type D: Category;
+    fn send_objects(&self, c: <<Self as Functor>::C as Category>::Object) -> <<Self as Functor>::D as Category>::Object;
+    fn send_arrows(&self, c: <<Self as Functor>::C as Category>::Arrow) -> <<Self as Functor>::D as Category>::Arrow;
 }
 
-fn insert_arrow(arrows: &mut Arrows, name: &str, domain: &str, codomain: &str) {
-    arrows.insert(name.to_string(), (domain.to_string(), codomain.to_string()));
-}
+// #[derive(PartialEq, Eq, Hash)]
+// struct CategoryObject {}
+//
+// #[derive(PartialEq, Eq, Hash)]
+// struct CategoryArrow {}
+//
+// struct Zero {}
+//
+// impl Category for Zero {
+//     type Object = CategoryObject;
+//     type Arrow = CategoryArrow;
+//
+//     fn domain(&self, _f: &CategoryArrow) -> CategoryObject { CategoryObject {} }
+//     fn codomain(&self, _f: &CategoryArrow) -> CategoryObject { CategoryObject {} }
+//     fn identity(&self, _a: CategoryObject) -> CategoryArrow { CategoryArrow {} }
+//     fn composition_internal(&self, _f: &CategoryArrow, _g: &CategoryArrow) -> CategoryArrow { CategoryArrow {} }
+//     fn objects() -> HashSet<CategoryObject> { HashSet::new() }
+//     fn arrows() -> HashSet<CategoryArrow> { HashSet::new() }
+// }
 
-fn check_isomorphism(category: Category, object1: &str, object2: &str) {
-    println!("{} and {} are isomorphism: {:?}", object1, object2, category.is_isomorphism(object1, object2));
-}
+struct Zero {}
 
-fn new_category(objects: Vec<&str>, arrow_names: Vec<(&str, &str, &str)>) -> Category {
-    let mut arrows = HashMap::new();
-    for arrow in &arrow_names {
-        insert_arrow(&mut arrows, arrow.0, arrow.1, arrow.2);
-    }
-    let category = Category { objects: objects.iter().map(|s| s.to_string()).collect(), arrows: arrows };
-    println!("made {}", category);
-    category
+impl Category for ZeroUnit {
+    type Object = ();
+    type Arrow = ();
+
+    fn domain(&self, _f: &()) {}
+    fn codomain(&self, _f: &()) {}
+    fn identity(&self, _a: ()) {}
+    fn composition_internal(&self, _f: &(), _g: &()) {}
+    fn objects() -> HashSet<()> { HashSet::new() }
+    fn arrows() -> HashSet<()> { HashSet::new() }
 }
 
 fn main() {
-    let category1 = cat!(
-        [A B C]
-        [f: A -> B
-         g: A -> C
-         h: B -> C]
-    );
-    check_isomorphism(category1, "A", "B");
-
-    let category2 = cat!(
-        [A B]
-        [f: A -> B
-         g: B -> A]
-    );
-    check_isomorphism(category2, "A", "B");
+    let zero = ZeroUnit {};
+    println!("{:?}", "hello category!");
 }
---
+++ src/main_old.rs
@@ -0,0 +1,79 @@
+macro_rules! cat {
+    ([ $( $object:ident )* ] [ $( $arrow_name:ident : $domain:ident -> $codomain:ident )* ]) => ({
+        let mut objects = Vec::new();
+        $( objects.push(stringify!($object)); )*
+        let mut arrows = Vec::new();
+        $( arrows.push((stringify!($arrow_name), stringify!($domain), stringify!($codomain))); )*
+        new_category(objects, arrows)
+    })
+}
+
+use std::fmt::Display;
+use std::collections::HashMap;
+
+type Arrows = HashMap<String, (String, String)>;
+
+struct Category {
+    objects: Vec<String>,
+    arrows: Arrows
+}
+
+impl Category {
+    fn has_arrow(&self, domain: &str, codomain: &str) -> bool {
+        for (dom, cod) in self.arrows.values() {
+            if dom == domain && cod == codomain {
+                return true
+            }
+        }
+        false
+    }
+
+    fn is_isomorphism(&self, object1: &str, object2: &str) -> bool {
+        self.has_arrow(object1, object2) && self.has_arrow(object2, object1)
+    }
+}
+
+impl Display for Category {
+    fn fmt(&self, dest: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut arrow_texts = vec![];
+        for (k, v) in &self.arrows {
+            arrow_texts.push(format!("{}: {} -> {}", k, v.0, v.1));
+        }
+        write!(dest, "objects: {:?} \n arrows: {:?}", self.objects, arrow_texts)
+    }
+}
+
+fn insert_arrow(arrows: &mut Arrows, name: &str, domain: &str, codomain: &str) {
+    arrows.insert(name.to_string(), (domain.to_string(), codomain.to_string()));
+}
+
+fn check_isomorphism(category: Category, object1: &str, object2: &str) {
+    println!("{} and {} are isomorphism: {:?}", object1, object2, category.is_isomorphism(object1, object2));
+}
+
+fn new_category(objects: Vec<&str>, arrow_names: Vec<(&str, &str, &str)>) -> Category {
+    let mut arrows = HashMap::new();
+    for arrow in &arrow_names {
+        insert_arrow(&mut arrows, arrow.0, arrow.1, arrow.2);
+    }
+    let category = Category { objects: objects.iter().map(|s| s.to_string()).collect(), arrows: arrows };
+    println!("made {}", category);
+    category
+}
+
+fn main() {
+    let category1 = cat!(
+        [A B C]
+        [f: A -> B
+         g: A -> C
+         h: B -> C]
+    );
+    check_isomorphism(category1, "A", "B");
+
+    let category2 = cat!(
+        [A B]
+        [f: A -> B
+         g: B -> A]
+    );
+    check_isomorphism(category2, "A", "B");
+}
